0cd118176e947ce0202ec465b7fcfdf0d1e176c9
Alexander <alexander185@gmail.com>
2023-08-18 12:07:17 +0300

    add some charts

--- src/data.ts
+++ src/data.ts
@@ -98,3 +98,125 @@ export const menu = [
         ]
     },
 ]
+
+export const topDealUsers = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=843&q=80",
+        username: "Elva McDonald",
+        email: "elva@gmail.com",
+        amount: "3.668",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80",
+        username: "Linnie Nelson",
+        email: "linnie@gmail.com",
+        amount: "3.256",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1583864697784-a0efc8379f70?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=688&q=80",
+        username: "Brent Reeves",
+        email: "brent@gmail.com",
+        amount: "2.998",
+    },
+    {
+        id: 4,
+        img: "https://images.unsplash.com/photo-1664575600850-c4b712e6e2bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        username: "Adeline Watson",
+        email: "adeline@gmail.com",
+        amount: "2.512",
+    },
+    {
+        id: 5,
+        img: "https://plus.unsplash.com/premium_photo-1689551670902-19b441a6afde?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80",
+        username: "Juan Harrington",
+        email: "juan@gmail.com",
+        amount: "2.134",
+    },
+    {
+        id: 6,
+        img: "https://images.unsplash.com/photo-1600878459138-e1123b37cb30?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=706&q=80",
+        username: "Augusta McGee",
+        email: "augusta@gmail.com",
+        amount: "1.932",
+    },
+    {
+        id: 7,
+        img: "https://images.unsplash.com/photo-1561948955-570b270e7c36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=601&q=80",
+        username: "Angel Thomas",
+        email: "angel@gmail.com",
+        amount: "1.560",
+    },
+];
+
+export const chartBoxUser = {
+    color: "#8884d8",
+    icon: "/userIcon.svg",
+    title: "Total Users",
+    number: "11.238",
+    dataKey: "users",
+    percentage: 45,
+    chartData: [
+        {name: "Sun", users: 400},
+        {name: "Mon", users: 600},
+        {name: "Tue", users: 500},
+        {name: "Wed", users: 700},
+        {name: "Thu", users: 400},
+        {name: "Fri", users: 500},
+        {name: "Sat", users: 450},
+    ],
+};
+
+export const chartBoxProduct = {
+    color: "skyblue",
+    icon: "/productIcon.svg",
+    title: "Total Products",
+    number: "238",
+    dataKey: "products",
+    percentage: 21,
+    chartData: [
+        {name: "Sun", products: 400},
+        {name: "Mon", products: 600},
+        {name: "Tue", products: 500},
+        {name: "Wed", products: 700},
+        {name: "Thu", products: 400},
+        {name: "Fri", products: 500},
+        {name: "Sat", products: 450},
+    ],
+};
+export const chartBoxRevenue = {
+    color: "teal",
+    icon: "/revenueIcon.svg",
+    title: "Total Revenue",
+    number: "$56.432",
+    dataKey: "revenue",
+    percentage: -12,
+    chartData: [
+        {name: "Sun", revenue: 400},
+        {name: "Mon", revenue: 600},
+        {name: "Tue", revenue: 500},
+        {name: "Wed", revenue: 700},
+        {name: "Thu", revenue: 400},
+        {name: "Fri", revenue: 500},
+        {name: "Sat", revenue: 450},
+    ],
+};
+export const chartBoxConversion = {
+    color: "gold",
+    icon: "/conversionIcon.svg",
+    title: "Total Ratio",
+    number: "2.6",
+    dataKey: "ratio",
+    percentage: 12,
+    chartData: [
+        {name: "Sun", ratio: 400},
+        {name: "Mon", ratio: 600},
+        {name: "Tue", ratio: 500},
+        {name: "Wed", ratio: 700},
+        {name: "Thu", ratio: 400},
+        {name: "Fri", ratio: 500},
+        {name: "Sat", ratio: 450},
+    ],
+};
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,8 +1,20 @@
 import "./home.scss"
+import TopBox from "../../components/topBox/TopBox.tsx";
+import ChartBox from "../../components/chartBox/ChartBox.tsx";
+import {chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser} from "../../data";
+
 const Home = () => {
     return (
         <div className={"home"}>
-            Home
+            <div className="box box1"><TopBox/></div>
+            <div className="box box2"><ChartBox {...chartBoxUser}/></div>
+            <div className="box box3"><ChartBox {...chartBoxProduct}/></div>
+            <div className="box box4">box4</div>
+            <div className="box box5"><ChartBox {...chartBoxConversion}/></div>
+            <div className="box box6"><ChartBox {...chartBoxRevenue}/></div>
+            <div className="box box7">Box7</div>
+            <div className="box box8">Box8</div>
+            <div className="box box9">Box9</div>
         </div>
     );
 };
